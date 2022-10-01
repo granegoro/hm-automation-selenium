683a6f5c78db447f277e9da0e890f798ffe0df95
Егор Старшелюк <granegoro@gmail.com>
2022-10-01 17:11:33 +0300

    Headless enabled

--- src/test/java/SubmitFormTest.java
+++ src/test/java/SubmitFormTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
@@ -109,11 +110,11 @@ class SubmitFormTest {
     @Test
     void ShouldNotPassWithoutAgreement() {
 
-       /* ChromeOptions options = new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver(options);*/
+        driver = new ChromeDriver(options);
 
         driver.get("http://localhost:9999/");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петр Петров");
