fb2fbdfbc8bb5f2d9bf53cd3ceec6f5ab0b0a09d
Егор Старшелюк <granegoro@gmail.com>
2022-10-01 17:51:21 +0300

    One test TestRun5

--- src/test/java/SubmitFormTest.java
+++ src/test/java/SubmitFormTest.java
@@ -21,7 +21,7 @@ class SubmitFormTest {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        /*driver = new ChromeDriver();*/
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
