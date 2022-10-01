894afe1d388c40494e864c96514fc2c7a8f8cc9d
Егор Старшелюк <granegoro@gmail.com>
2022-10-01 17:02:26 +0300

    Autotests added, error expected

--- src/test/java/SubmitFormTest.java
+++ src/test/java/SubmitFormTest.java
@@ -1,8 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
 
 
 class SubmitFormTest {
@@ -24,27 +27,102 @@ class SubmitFormTest {
     }
 
     @Test
-    void test() {
-        ChromeOptions options = new ChromeOptions();
+    void ShouldFillAndSendSuccessfully() {
+
+        /*ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);*/
 
         driver.get("http://localhost:9999/");
-        System.out.println();
-        //driver.findElement().sendKeys("Петр Петров");
-        //driver.findElement().sendKeys("+79999999999");
-        //driver.findElement().click();
-        //driver.findElement().click();
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петр Петров");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
 
-       // String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-       // String actual = driver.findElement().getText();
-       // Assertions.assertEquals(expected, actual);
-
-
-
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    void ShouldFillAndSendSuccessfullyWithDashInName() {
+
+       /* ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);*/
+
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Жан-Жак Петров");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void ShouldNotPassWithIncorrectName() {
+
+       /* ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);*/
+
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Willy Dilly");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name]")).findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void ShouldNotPassWithIncorrectPhone() {
+
+       /* ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);*/
+
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петр Петров");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+7999");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone]")).findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void ShouldNotPassWithoutAgreement() {
+
+       /* ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);*/
+
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петр Петров");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        driver.findElement(By.className("button")).click();
+
+        String expected = "Я сглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+        String actual = driver.findElement(By.className("input_invalid")).getText();
+        Assertions.assertEquals(expected, actual);
+    }
 
 }
